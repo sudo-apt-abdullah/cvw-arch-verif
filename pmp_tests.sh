073e3028bf305f54bdce490badced1c3ac985ee3
Mysterio-Abdullah <mrabdullahmib381@gmail.com>
2025-05-14 08:34:23 -0700

    cp_pmp64 and generalize the walking ones cp

---
+++ pmp_tests.sh
@@ -0,0 +1,1 @@
+wsim rv32gc /home/mabdullab/cvw/tests/riscof/work/riscv-arch-test/rv32i_m/pmp32/src/pmp-CFG-reg.S/ref/ref.elf --fcov
---
+++ vm_tests.sh
@@ -0,0 +1,2 @@
+wsim rv32gc /home/mabdullab/cvw/tests/riscof/work/riscv-arch-test/rv32i_m/vm_sv32/src/mstatus_tvm_test.S/ref/ref.elf --fcov
+wsim rv32gc /home/mabdullab/cvw/tests/riscof/work/riscv-arch-test/rv32i_m/vm_sv32/src/pmp_check_on_pa_S_mode.S/ref/ref.elf --fcov
